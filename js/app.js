f1c10eb4ef41c93baad3b1e2fa2926989ae252b2
Tricia Sawyer <tricia.sawyer03@gmail>
2023-03-14 18:22:28 -0500

    edit errors in javascript

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
-"use strict";
+'use strict';
 console.log('app file is working');
 
 
 
-let whatName = prompt("What is your name?");
+let whatName = prompt('What is your name?');
 //console.log("🚀 ~ file: index.html:34 ~ whatName:", whatName);
 alert('Nice to meet you ' + whatName + ', welcome to my website!');
 
-let  onlyChild = prompt("Am I an only child?").toLowerCase();
+let onlyChild = prompt('Am I an only child?').toLowerCase();
 //console.log("🚀 ~ file: index.html:36 ~ onlyChild:", onlyChild);
 if (onlyChild === 'no' || onlyChild === 'n') {
-    alert('You got it! I actually have 3 siblings.');
+  alert('You got it! I do have 3 siblings.');
 } else if (onlyChild === 'yes' || onlyChild === 'y') {
-    alert('Gotcha! I have 3 siblings.');
+  alert('Wrong! I actually have 3 siblings.');
 }
 
-let anyPets = prompt("Do I have any pets?").toLowerCase();
+let anyPets = prompt('Do I have any pets?').toLowerCase();
 //console.log("🚀 ~ file: index.html:38 ~ anyPets:", anyPets);
 if (anyPets === 'no' || anyPets === 'n') {
-    alert('Correct! I sadly do not have any pets.');
+  alert('Correct! I sadly do not have any pets.');
 } else if (anyPets === 'yes' || anyPets === 'y') {
-    alert('Nope!');
+  alert('Nope!');
 }
 
-let inAliens = prompt("Do I believe in aliens?").toLowerCase();
+let inAliens = prompt('Do I believe in aliens?').toLowerCase();
 //console.log("🚀 ~ file: index.html:40 ~ inAliens:", inAliens);
 if (inAliens === 'yes' || inAliens === 'y') {
-    alert('Oh yeah! I definitely do');
-} else {
-    alert('Wrong! I do.');
+  alert('Oh yeah! I definitely do');
+} else if (inAliens === 'no' || inAliens === 'n') {
+  alert('Wrong! Aliens are real.');
 }
