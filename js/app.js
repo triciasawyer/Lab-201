2bb9de06f42325d332b418319809ff75da1d9c10
Tricia Sawyer <tricia.sawyer03@gmail>
2023-03-14 14:19:15 -0500

    add javascript and more content for webpage

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 "use strict";
-console.log ('app file is working');
+console.log('app file is working');
 
 
 
-let favColor = prompt("Is my favorite color blue? Answer yes or no!");
-console.log("🚀 ~ file: index.html:34 ~ favColor:", favColor);
-if(lowerCaseInput === 'yes' || lowerCaseInput === y){
-    alert ('Right on! My favorite color is blue');
-} else {
-    alert ('Shoot, my favorite color is blue!');
-}
-
+let whatName = prompt("What is your name?");
+console.log("🚀 ~ file: index.html:34 ~ whatName:", whatName);
+alert('Nice to meet you ' + whatName + ', welcome to my website!');
 
 let onlyChild = prompt("Am I an only child?");
 console.log("🚀 ~ file: index.html:36 ~ onlyChild:", onlyChild);
-if(lowerCaseInput === 'no' || lowerCaseInput === n){
-    alert ('You got it! I actually have 3 siblings.');
-} else {
-    alert ('Oh no, you got it wrong!');
+let lowerCase = onlyChild.toLowerCase();
+console.log("🚀 ~ file: app.js:14 ~ log:", log)
+if (onlyChild === 'no' || onlyChild === 'n') {
+    alert('You got it! I actually have 3 siblings.');
+} else if (onlyChild === 'yes' || onlyChild === 'y') {
+    alert('Gotcha! I have 3 siblings.');
 }
-
 
 let anyPets = prompt("Do I have any pets?");
 console.log("🚀 ~ file: index.html:38 ~ anyPets:", anyPets);
-if(lowerCaseInput === 'no' || lowerCaseInput === n){
-    alert ('Correct! I sadly do not have any pets.');
-} else {
-    alert ('Shoot, my favorite color is blue!');
+if (anyPets === 'no' || anyPets === 'n') {
+    alert('Correct! I sadly do not have any pets.');
+} else if (anyPets === 'yes' || anyPets === 'y') {
+    alert('Nope!');
 }
-
 
 let inAliens = prompt("Do I believe in aliens?");
 console.log("🚀 ~ file: index.html:40 ~ inAliens:", inAliens);
+if (inAliens === 'yes' || inAliens === 'y') {
+    alert('Oh yeah! I definitely do');
+} else {
+    alert('Wrong! I do.');
+}
