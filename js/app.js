643d1897a6e19e63d813d7a2ecbfd6694d6faeee
Tricia Sawyer <tricia.sawyer03@gmail>
2023-03-15 10:49:47 -0500

    edit javascript

--- js/app.js
+++ js/app.js
@@ -3,12 +3,32 @@ console.log('app file is working');
 
 
 
-let whatName = prompt('What is your name?');
+//let userPoints = 0;
+//console.log("🚀 ~ file: app.js:5 ~ userPoints:", userPoints);
+
+
+//let userName;
+
+//while(!userName) {
+// userName = prompt('Hello, What is your name');
+//}
+
+
+
+let whatName = prompt('Hello, what is your name?');
 //console.log("🚀 ~ file: index.html:34 ~ whatName:", whatName);
 alert('Nice to meet you ' + whatName + ', welcome to my website!');
+alert('Here is a quick game of questions About Me. Just answer yes or no!');
 
+let favSport = prompt('Is my favorite sport basketball?').toLowerCase().trim();
+//console.log("🚀 ~ file: index.html:36 ~ favSport:", favSport);
+if (favSport === 'no' || favSport === 'n') {
+  alert('Your right! My favorite sport is baseball.');
+} else if (favSport === 'yes' || favSport === 'y') {
+  alert('Wrong! I like baseball.');
+}
 
-let onlyChild = prompt('Am I an only child?').toLowerCase();
+let onlyChild = prompt('Am I an only child?').toLowerCase().trim();
 //console.log("🚀 ~ file: index.html:36 ~ onlyChild:", onlyChild);
 if (onlyChild === 'no' || onlyChild === 'n') {
   alert('You got it! I do have 3 siblings.');
@@ -16,7 +36,7 @@ if (onlyChild === 'no' || onlyChild === 'n') {
   alert('Wrong! I actually have 3 siblings.');
 }
 
-let anyPets = prompt('Do I have any pets?').toLowerCase();
+let anyPets = prompt('Do I have any pets?').toLowerCase().trim();
 //console.log("🚀 ~ file: index.html:38 ~ anyPets:", anyPets);
 if (anyPets === 'no' || anyPets === 'n') {
   alert('Correct! I sadly do not have any pets.');
@@ -24,7 +44,7 @@ if (anyPets === 'no' || anyPets === 'n') {
   alert('Nope!');
 }
 
-let inAliens = prompt('Do I believe in aliens?').toLowerCase();
+let inAliens = prompt('Do I believe in aliens?').toLowerCase().trim();
 //console.log("🚀 ~ file: index.html:40 ~ inAliens:", inAliens);
 if (inAliens === 'yes' || inAliens === 'y') {
   alert('Oh yeah! I definitely do');
