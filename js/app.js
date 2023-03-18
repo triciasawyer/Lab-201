64e458cdb7b81412542494230181cb79f5d0dbf5
Tricia Sawyer <tricia.sawyer03@gmail>
2023-03-17 21:10:22 -0500

    remove consol logs and edit things for lab 5b

--- js/app.js
+++ js/app.js
@@ -3,18 +3,15 @@ console.log('app file is working');
 
 
 let userPoints = 0;
-console.log('🚀 ~ file: app.js:5 ~ userPoints:', userPoints);
 
 
 let whatName = prompt('Hello, what is your name?');
-console.log('🚀 ~ file: index.html:34 ~ whatName:', whatName);
 alert('Nice to meet you ' + whatName + ', welcome to my website!');
 alert('Here is a quick game of questions About Me. Just answer yes or no!');
 
 
 function runSport(){
   let favSport = prompt('Is my favorite sport basketball?').toLowerCase().trim();
-  console.log('🚀 ~ file: index.html:36 ~ favSport:', favSport);
   if (favSport === 'no' || favSport === 'n') {
     alert('Your right! My favorite sport is baseball.');
     userPoints++;
@@ -27,7 +24,6 @@ runSport();
 
 function runChild(){
   let onlyChild = prompt('Am I an only child?').toLowerCase().trim();
-  console.log('🚀 ~ file: index.html:36 ~ onlyChild:', onlyChild);
   if (onlyChild === 'no' || onlyChild === 'n') {
     alert('You got it! I do have 3 siblings.');
     userPoints++;
@@ -40,7 +36,6 @@ runChild();
 
 function runPets(){
   let anyPets = prompt('Do I have any pets?').toLowerCase().trim();
-  console.log('🚀 ~ file: index.html:38 ~ anyPets:', anyPets);
   if (anyPets === 'no' || anyPets === 'n') {
     alert('Correct! I sadly do not have any pets.');
     userPoints++;
@@ -53,7 +48,6 @@ runPets();
 
 function runAliens(){
   let inAliens = prompt('Do I believe in aliens?').toLowerCase().trim();
-  console.log('🚀 ~ file: index.html:40 ~ inAliens:', inAliens);
   if (inAliens === 'yes' || inAliens === 'y') {
     alert('Oh yeah! I definitely do');
     userPoints++;
@@ -91,7 +85,6 @@ function runTraveled(){
   while (chances < 6) {
     chances++;
     let multiAnswer = prompt('What states have I traveled to? You get six attempts.').toLowerCase().trim();
-    console.log('🚀 ~ file: app.js:65 ~ multiAnswer:', multiAnswer);
 
     if (statesTraveled.includes(multiAnswer)) {
       alert('You guessed one of the states I have traveled too! Well done.');
@@ -103,21 +96,3 @@ function runTraveled(){
   alert('Thanks for playing my About Me game ' + whatName + '! Your score was ' + userPoints);
 }
 runTraveled();
-
-
-
-
-// Factoring random number in guessing number game
-// let randomNumber = math.random() * 10;  || gives decimal number
-// let randomNumber = math.floor(math.random() * 10) + 1 ; || whole number, math.floor gives us whole number. The +1 gives us a range
-// let attempt = 4;
-// let question = 'guess an integer from 1-10';
-// let guessNumber = parseInt(prompt(question = 'You have' + attemtps + 'try\'s to go.'))
-// if they don't want to play
-//if (!guessNumber){
-//  break;
-//}
-//if (guessNumber === randomNumber) {
-// alert('You guessed  the correct number, ' + randomNumber)
-// }
-
